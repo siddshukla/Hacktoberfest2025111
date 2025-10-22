5b98d28c4c53e3687c528dfd2c71970c36e18dca
siddharth shukla <124035361+siddshukla@users.noreply.github.com>
2025-10-22 13:23:12 +0530

    Add Java program to reverse digits of a number
    This program defines a method to reverse the digits of an integer and prints the reversed number for a given input.

---
+++ demo.java
@@ -0,0 +1,16 @@
+// Java program to reverse a number
+class demo {
+    static int reverseDigits(int n) {
+        int revNum = 0;
+        while (n > 0) {
+            revNum = revNum * 10 + n % 10;
+            n = n / 10;
+        }
+        return revNum;
+    }
+  
+    public static void main(String[] args) {
+        int num = 4562;
+        System.out.println(reverseDigits(num));
+    }
+}
